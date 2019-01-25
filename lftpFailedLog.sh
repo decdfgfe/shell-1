5f9b51f0b6d3be96d9792601a4564385903407b4
rzx001 <46742377+rzx001@users.noreply.github.com>
2019-01-25 18:15:39 +0800

    Create lftpFailedLog.sh

---
+++ lftpFailedLog.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+mylog(){
+         echo `date '+%F %H:%M:%S'` $@ >>/var/log/ftp.log
+        }
+failedLogpath=/appslog/log/inbusin/ecenter/fail
+user=sa
+password=sa@1218
+mac=00-E0-4C-1B-F7-31
+case $1 in
+        00)
+        event=onoffevent
+        ;;
+        05)
+        event=aevent
+        ;;
+        06)
+        event=fevent
+        ;;
+        07)
+        event=binary
+        ;;
+        *)
+        echo "Usage: `basename $0` [00|05|06|07]"
+        exit 1
+        ;;
+esac
+if [ $(ls -tr $failedLogpath/*-${1}-*.zip | wc -l) -gt 0 ];then
+for file in $(ls -at $failedLogpath/*-${1}-*.zip) ;do
+lftp -u ${user},${password} ftp://10.12.109.132<<EOF
+cd ${event}/${mac}
+put $file
+close
+bye
+EOF
+if [ $? -eq 0 ];then
+        rm -f $file
+        if [ $? -eq 0 ];then
+                mylog $file ok,rm ok
+        else
+                mylog $file ok
+        fi
+fi
+done
+fi
