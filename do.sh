3c3808cdea7cc813d411c923f68c293177e7e553
rzx001 <46742377+rzx001@users.noreply.github.com>
2019-01-26 11:21:20 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while true;do
         for code in 00 05 06 07;do
-                nohup bash lftp_failed_log_v2.sh >/dev/null 2>&1
+                nohup bash lftp_failed_log_v2.sh >/dev/null 2>&1 &
         done
+
 done
