84b22050c8323fd10e0cc9fb2a9ad004595e1aa3
rzx001 <46742377+rzx001@users.noreply.github.com>
2019-01-26 13:54:18 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # usage nohup bash do.sh &
+# suggest use suppervisor to monitor the script
 while true;do
-        for code in 00 05 06 07;do
-                nohup bash lftp_failed_log_v2.sh >/dev/null 2>&1 &
-        done
-sleep 3600
+        if [ $(ls -l /appslog/log/inbusin/ecenter/fail/ | wc -l) -gt 0 ] ;then
+                for code in 00 05 06 07;do
+                        nohup bash lftp_failed_log_v2.sh ${code} >/dev/null 2>&1 &
+                done
+        sleep 10
+        else
+                echo `date '+%F %H:%M:%S'` "logfile empty,no need to transfer" >>/var/log/ftp.log
+        fi
 done
