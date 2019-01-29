47ec130aad9c6344514940fd14b3827d694cabe5
rzx001 <46742377+rzx001@users.noreply.github.com>
2019-01-29 17:38:11 +0800

    Update doreg.sh

--- doreg.sh
+++ doreg.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # transfer reginfo_rtx.zip to reg server and do reg opration
+info(){
+        echo -e "\033[31m$@\033[0m"
+}
 zipfile=reginfo_rtx.zip
 [ -f $zipfile ] && {
+        info "copy ${zipfile} to remote server"
         scp -P 1218 $zipfile rzxsystemuser@192.168.50.95:/tmp/
 }
+info "execute reg.sh"
 ssh rzxsystemuser@192.168.50.95 -p 1218 "cd /tmp/ && bash /apps/rtx/run_env/script/tool/reg.sh"
 
 if [ $? -eq 0 ];then
+        info "copy rzxregcode.rc to local directory"
         scp -P 1218 rzxsystemuser@192.168.50.95:/tmp/rzxregcode.rc .
-        ssh rzxsystemuser@192.168.50.95 -p 1218 "rm -f /tmp/$zipfile; rm -f rzxregcode.rc"
+        info "rm ${zipfile} and rzxregcode.rc"
+        ssh rzxsystemuser@192.168.50.95 -p 1218 "rm -f /tmp/${zipfile} && rm -f /tmp/rzxregcode.rc"
 fi
